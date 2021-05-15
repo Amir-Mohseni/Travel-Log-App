4a2c1ecb2907fdb3d64e6687e663ab08b68ac029
Amir-Mohseni <iliya.numb@gmail.com>
2021-05-15 23:14:27 +0430

    Added Footer.js file manually

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      Made By <a target="_blank" href="https://amirmohseni.dev"> Amir Mohseni</a>
+    </div>
+  )
+}
