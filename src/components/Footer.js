3effb9cd2a812eee4a073b4361cefd9f13fb4f18
Amir-Mohseni <iliya.numb@gmail.com>
2021-05-15 23:27:44 +0430

    Removed target blank from footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer">
-      Made By <a target="_blank" href="https://amirmohseni.dev"> Amir Mohseni</a>
+      Made By <a href="https://amirmohseni.dev"> Amir Mohseni</a>
     </div>
   )
 }
